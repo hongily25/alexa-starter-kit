2e7e7ad658f723b0619c70babe0c3e969d495ab0
Emily Lam <hlam93@gmail.com>
2018-01-30 10:40:12 -0800

    Update lambda.js

--- lambda.js
+++ lambda.js
@@ -1,23 +1,16 @@
-/* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
-
 'use strict';
 
 const Alexa = require('alexa-sdk');
 
-const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+const APP_ID = undefined;
 
 const handlers = {
     'LaunchRequest': function () {
-        const welcomeMsg = "Welcome to Have You Ever. Say give me a question to start the game or stop to stop the game.";
-        this.emit(':askWithCard', welcomeMsg, this.t('HELP_REPROMPT'), this.t('SKILL_NAME'), '... ');
+        const welcomeMsg = "Welcome.";
+        const reprompt = "I didn't get that. Can you repeat that?";
+        const cardTitle = "My Skill Name";
+        const cardBody = "Sample Output for Card Body";
+        this.emit(':askWithCard', welcomeMsg, reprompt, cardTitle, cardBody);
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = "How can I help you?";
@@ -34,8 +27,6 @@ const handlers = {
 exports.handler = function (event, context) {
     const alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
